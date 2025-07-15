daac733d2f2cb03299d20d93fe3ccc0133f8210f
Tatiane Lopes <tatiane.lopes@ufla.br>
2025-07-15 11:21:02 -0300

    Inclusão metadado description e Correção bug função data

--- ui/src/app/item-page/field-components/metadata-values/metadata-values.component.ts
+++ ui/src/app/item-page/field-components/metadata-values/metadata-values.component.ts
@@ -146,15 +146,14 @@ export class MetadataValuesComponent implements OnChanges {
       return { target: '_blank', rel: 'noopener noreferrer' };
     }
   }
-
+/**Função para formação de data BR */
   isDate(value: string): string {
-    // return !isNaN(Date.parse(value));
-  const date = new Date(value);
-  if (isNaN(date.getTime())) {
-    return value.trim() // Não é uma data válida
+    const regex = /^\d{4}-\d{2}-\d{2}$/;
+    if (regex.test(value)) {
+      const [year, month, day] = value.split("-");
+      return `${day}/${month}/${year}`;
+    }
+  
+    return value.trim();
   }
-
-  // Formata para o padrão brasileiro: dd/mm/aaaa
-  return date.toLocaleDateString('pt-BR');
-}
 }
