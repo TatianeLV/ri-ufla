7a4a183a9b2afd18584103307474b46d6f177823
Tatiane Lopes <tatiane.lopes@ufla.br>
2026-08-25 15:27:18 -0300

    Restaura configuracao funcional dos links Lattes e ORCID

--- ui/src/themes/ufla/app/item-page/simple/item-types/untyped-item/untyped-item.component.ts
+++ ui/src/themes/ufla/app/item-page/simple/item-types/untyped-item/untyped-item.component.ts
@@ -37,8 +37,8 @@ import { ThemedThumbnailComponent } from '../../../../../../../app/thumbnail/the
 @Component({
   selector: 'ds-untyped-item',
   // styleUrls: ['./untyped-item.component.scss'],
-    styleUrls: [
-      '../../../../../../app/item-page/simple/item-types/untyped-item/untyped-item.component.scss',
+  styleUrls: [
+    '../../../../../../../app/item-page/simple/item-types/untyped-item/untyped-item.component.scss',
   ],
   templateUrl: './untyped-item.component.html',
   // templateUrl:
