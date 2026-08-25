d29a809487e5904bf6d3377befb04897f90f216e
Tatiane Lopes <tatiane.lopes@ufla.br>
2026-08-25 14:44:38 -0300

    Corrige estilo dos links Lattes e ORCID

--- ui/src/themes/ufla/app/item-page/simple/item-types/untyped-item/untyped-item.component.ts
+++ ui/src/themes/ufla/app/item-page/simple/item-types/untyped-item/untyped-item.component.ts
@@ -39,7 +39,6 @@ import { ThemedThumbnailComponent } from '../../../../../../../app/thumbnail/the
   // styleUrls: ['./untyped-item.component.scss'],
     styleUrls: [
       '../../../../../../app/item-page/simple/item-types/untyped-item/untyped-item.component.scss',
-      './untyped-item.component.scss',
   ],
   templateUrl: './untyped-item.component.html',
   // templateUrl:
